878cf46285c297e00f7de9b9823f52650882f522
nickyim <nicholasjyim@gmail.com>
2024-09-10 16:16:34 -0400

    ga4 update

--- ghostlink-frontend/src/app/layout.tsx
+++ ghostlink-frontend/src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 
           {/* Google Analytics */}
           <Script
-            strategy="afterInteractive"
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
           <Script
